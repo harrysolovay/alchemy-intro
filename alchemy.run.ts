b50f8fd8aaddb436a3c605b7bbafed6f88f2ce6d
Harry Solovay <harrysolovay@gmail.com>
2025-08-16 12:56:27 -0400

    configure ci

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,13 +1,30 @@
-import alchemy from "alchemy"
-import {Worker} from "alchemy/cloudflare"
+import alchemy from "alchemy";
+import { Worker } from "alchemy/cloudflare";
+import { GitHubComment } from "alchemy/github";
+import { CloudflareStateStore } from "alchemy/state";
+import { env } from "node:process";
 
-const app = await alchemy("my-app")
+const app = await alchemy("my-app", {
+  stateStore: (scope) => new CloudflareStateStore(scope),
+});
 
 const worker = await Worker("my-worker", {
   entrypoint: "./worker.ts",
-})
+  version: app.stage === "prod" ? undefined : app.stage,
+});
 
-const {url} = worker
-console.log({url})
+const { url } = worker;
+console.log({ url });
 
-await app.finalize()
+if (env.PULL_REQUEST) {
+  await GitHubComment("preview-comment", {
+    owner: "harrysolovay",
+    repository: "alchemy-intro",
+    issueNumber: Number(env.PULL_REQUEST),
+    body: `Preview URL: ${url}
+    
+Built from ${env.GITHUB_SHA!.slice(0, 7)}`
+  })
+}
+
+await app.finalize();
